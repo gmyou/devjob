05d7649cdd300227cecc2a05248161208b8802e3
ec2-user <gmyou71@gmail.com>
2013-05-28 03:46:36 +0000

    add extract IP

---
+++ test/extractIP.py
@@ -0,0 +1,24 @@
+import datetime
+
+s = datetime.datetime.now()
+print("Start : "+str(s))
+
+f = open('localhost_access_log.20121024_stats.log')
+w = file('ip', 'w')
+i = 0
+
+for line in f:
+        arr = line.split()
+        w.write(arr[0])
+        w.write('\n')
+        i += 1
+
+f.close()
+w.close()
+
+e = datetime.datetime.now()
+print("End : "+str(e))
+
+print(
+
+print("Total. "+ str(i))
